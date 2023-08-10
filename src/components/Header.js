b04bc2815e9e5a4868ba54f6b53e614b567cf24f
Garvit-1 <garvitgarg200@gmail.com>
2023-08-11 02:00:42 +0530

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,83 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+    // margin: "auto",
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+const lightTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#D3D3D3",
+    },
+    type: "light",
+  },
+});
+
+function Header() {
+  const classes = useStyles();
+  const { currency, setCurrency } = CryptoState();
+
+  const history = useHistory();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            {/* <img src="./CW.png" className={classes.image}  alt="img" /> */}
+            <Typography
+              onClick={() => history.push(`/`)}
+              variant="h6"
+              className={classes.title}
+            >
+              CoinWatch
+            </Typography>
+            {/* <Button color="inherit">Login</Button> */}
+            <Select
+              variant="outlined"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={currency}
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD $</MenuItem>
+              <MenuItem value={"INR"}>INR ₹</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
+export default Header;
